ccd312a85b7acc60a131d46d4e6b67e4230c3db8
yanghaibo604520yhb <594917235@qq.com.com>
2025-12-10 13:16:14 +0800

    鍒涘缓鏈€绠€鍗曠殑 OpenNext TypeScript 閰嶇疆鏂囦欢

--- open-next.config.js
+++
@@ -1,8 +0,0 @@
-// OpenNext Cloudflare 配置文件
-// 使用 JavaScript 格式，避免 TypeScript 导入问题
-
-module.exports = {
-  // OpenNext 默认配置
-  // Cloudflare 适配器会自动处理大部分配置
-};
-
---
+++ open-next.config.ts
@@ -0,0 +1,5 @@
+// OpenNext Cloudflare 配置文件
+// 最简单的配置，使用默认设置
+
+export default {};
+
